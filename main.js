03896523c1cc5cce068cbd964a2a5a61693ead6f
mattot-the-builder <135392742+mattot-the-builder@users.noreply.github.com>
2023-11-24 07:07:26 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,69 @@
+export default {
+  menuGroup: [
+  {
+      groupLabel: "ADMIN",
+      menuItems: [
+        { label: "Konfigurasi" },
+        { label: "Carian Pengguna" },
+        { label: "Pengguna Aktif" },
+        { label: "Pengumuman" },
+        { label: "Galeri", icon: "image" },
+        { label: "Kaji Selidik", icon: "image" },
+      ],
+    },
+    {
+      groupLabel: "PRA PERKAHWINAN",
+      menuItems: [
+        { label: "Tajuk Kursus" },
+        { label: "Penceramah Kursus" },
+        { label: "Penganjur Kursus" },
+        { label: "Tempat Kursus" },
+        { label: "Jadual Kursus" },
+        { label: "Carian Peserta" },
+      ],
+    },
+    {
+      groupLabel: "PERMOHONAN NCR ONLINE",
+      menuItems: [
+        { label: "Kebenaran Berkahwin Online" },
+        { label: "Nikah Online" },
+        { label: "Cerai Online" },
+        { label: "Rujuk Online" },
+      ],
+    },
+    {
+      groupLabel: "PERMOHONAN NCR",
+      menuItems: [
+        { label: "Senarai Kerja" },
+        { label: "Kebenaran Berkahwin" },
+        { label: "Pra Daftar Nikah" },
+        { label: "Nikah" },
+        { label: "Cerai" },
+        { label: "Rujuk" },
+        { label: "NCR Backlog", subMenu: ["Carian", "NBC"] },
+        { label: "Hapus Permohonan" },
+        { label: "Cetakan Kad Nikah" },
+        { label: "Cetakan Daftar NCR" },
+        { label: "Sejarah NCR" },
+      ],
+    },
+    {
+      groupLabel: "NCR LUAR NEGARA",
+      menuItems: [
+        { label: "Senarai Kerja" },
+        { label: "Carian" },
+        { label: "Senarai Luar Negara" },
+      ],
+    },
+    {
+      groupLabel: "KHIDMAT NASIHAT",
+      menuItems: [
+        { label: "Aduan" },
+        { label: "Carian" },
+        { label: "Perunding" },
+        { label: "Aduan Online" },
+        { label: "Jawatan Kuasa Pendamai" },
+      ],
+    },
+  ],
+};
